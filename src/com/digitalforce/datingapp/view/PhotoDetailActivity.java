20c093b2eb6da86ef1a19befac44d194aa5a689a
mohammed.farhan <mohammed.farhan@jabong.com>
2014-10-31 18:09:10 +0530

    commit

---
+++ src/com/digitalforce/datingapp/view/PhotoDetailActivity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.digitalforce.datingapp.view;
+
+import android.os.Bundle;
+import com.digitalforce.datingapp.R;
+
+/**
+ * @author m.farhan
+ *
+ */
+public class PhotoDetailActivity extends BaseActivity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_detail_layout);
+	}
+
+	@Override
+	public void onEvent(int eventId, Object eventData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
